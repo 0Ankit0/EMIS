c9ea2cf471ef2db61c2a201a37a284c777e538c0
0ankit0 <ankitpaudel.1373@gmail.com>
2025-11-22 05:28:12 +0545

    refactor(exams): update exam app configuration
    - Update exam app initialization
    - Improve app configuration
    - Enhance module structure

--- apps/exams/apps.py
+++ apps/exams/apps.py
@@ -1,3 +1,6 @@
+"""
+Exams App Configuration
+"""
 from django.apps import AppConfig
 
 
@@ -5,3 +8,10 @@ class ExamsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.exams'
     verbose_name = 'Exams'
+    
+    def ready(self):
+        """Import signals when app is ready"""
+        try:
+            import apps.exams.signals
+        except ImportError:
+            pass
