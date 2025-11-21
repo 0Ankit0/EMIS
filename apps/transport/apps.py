e5a51944305236a066161154ca2ebad6f5602b09
0ankit0 <ankitpaudel.1373@gmail.com>
2025-11-22 05:30:26 +0545

    refactor(transport): update app configuration and serializers
    - Update transport app configuration
    - Add comprehensive serializers
    - Improve app initialization

--- apps/transport/apps.py
+++ apps/transport/apps.py
@@ -1,3 +1,6 @@
+"""
+Transport App Configuration
+"""
 from django.apps import AppConfig
 
 
@@ -5,3 +8,10 @@ class TransportConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.transport'
     verbose_name = 'Transport'
+    
+    def ready(self):
+        """Import signals when app is ready"""
+        try:
+            import apps.transport.signals
+        except ImportError:
+            pass
