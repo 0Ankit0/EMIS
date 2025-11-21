c8090d75041c0879ff8473f6d741fcd7f33083c8
0ankit0 <ankitpaudel.1373@gmail.com>
2025-11-22 05:30:20 +0545

    refactor(timetable): update app configuration and serializers
    - Update timetable app configuration
    - Add comprehensive serializers
    - Improve app initialization

--- apps/timetable/apps.py
+++ apps/timetable/apps.py
@@ -1,3 +1,6 @@
+"""
+Timetable App Configuration
+"""
 from django.apps import AppConfig
 
 
@@ -5,3 +8,10 @@ class TimetableConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.timetable'
     verbose_name = 'Timetable'
+    
+    def ready(self):
+        """Import signals when app is ready"""
+        try:
+            import apps.timetable.signals
+        except ImportError:
+            pass
