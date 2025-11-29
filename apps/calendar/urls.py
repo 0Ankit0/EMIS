8b90712f409b62150349986c1cf3acf94c414a6a
0ankit0 <ankitpaudel.1373@gmail.com>
2025-11-30 05:07:11 +0545

    feat(calendar): add DRF router and register Calendar, Category, and Event endpoints

---
+++ apps/calendar/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CalendarViewSet, EventViewSet, CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'calendars', CalendarViewSet)
+router.register(r'events', EventViewSet)
+router.register(r'categories', CategoryViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
