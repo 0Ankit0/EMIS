8fddf9851aed2b02a54c2b1fce361be21db70082
0ankit0 <ankitpaudel.1373@gmail.com>
2025-12-04 14:05:50 +0545

    config: register CurrentUserMiddleware in settings
    - Add CurrentUserMiddleware to MIDDLEWARE list
    - Enable user tracking across all requests
    - Support automated change tracking in models

--- apps/emis/settings.py
+++ apps/emis/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.core.middleware.CurrentUserMiddleware',
 ]
 
 ROOT_URLCONF = 'apps.emis.urls'
